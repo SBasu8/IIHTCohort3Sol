11b6bfadd52e909a2588a4b74bc4e55a2de13ae1
Prabin Rath <prabinrath123@gmail.com>
2020-09-13 11:17:48 +0530

    Upload Excel working !

--- StockChartingApp.UploadExcelMS/Controllers/UploadExcelController.cs
+++ StockChartingApp.UploadExcelMS/Controllers/UploadExcelController.cs
@@ -63,16 +63,9 @@ namespace StockChartingApp.UploadExcelMS.Controllers
             try
             {
                 var file = Request.Form.Files[0];
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), "UploadFiles");
                 if (file.Length > 0)
                 {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-                    repository.UploadExcel(fullPath);
+                    repository.UploadExcel(file);
                     return Ok();
                 }
                 else
--- StockChartingApp.UploadExcelMS/Repositories/UploadExcelRepository.cs
+++ StockChartingApp.UploadExcelMS/Repositories/UploadExcelRepository.cs
@@ -1,4 +1,6 @@
 ﻿using EntityLibraryStockChartingApp;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
 using StockChartingApp.UploadExcelMS.Models;
 using System;
 using System.Collections.Generic;
@@ -19,68 +21,34 @@ namespace StockChartingApp.UploadExcelMS.Repositories
             this.context = context;
         }
 
-        public void UploadExcel(string filePath)
+        public void UploadExcel(IFormFile file)
         {
             var list = new List<StockPrice>();
-            FileInfo info = new FileInfo(filePath);
-            string fileExtension = info.Extension;
-            string excelConString = "";
-            //Get connection string using extension
-            switch (fileExtension)
+            using (var stream = new MemoryStream())
             {
-                // If uploaded file is Excel 1997 - 2007.
-                case ".xls":
-                    excelConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
-                    break;
-                //If uploaded file is Excel 2007 and above
-                case ".xlsx":
-                    excelConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
-                    break;
-            }
-            // Read data from first sheet of excel into datatable
-            DataTable dt = new DataTable();
-            excelConString = string.Format(excelConString, filePath);
-
-            using (OleDbConnection excelOledbConnection = new OleDbConnection(excelConString))
-            {
-                using (OleDbCommand excelDbCommand = new OleDbCommand())
+                file.CopyTo(stream);
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (var pkg = new ExcelPackage(stream))
                 {
-                    using (OleDbDataAdapter excelDataAdapter = new OleDbDataAdapter())
+                    ExcelWorksheet ew = pkg.Workbook.Worksheets[0];
+                    var rCount = ew.Dimension.Rows;
+
+                    for (int row = 2; row <= rCount; row++)
                     {
-                        excelDbCommand.Connection = excelOledbConnection;
-
-                        excelOledbConnection.Open();
-                        // Get schema from excel sheet
-                        DataTable excelSchema = excelOledbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);                        // Get sheet name
-                        string sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
-                        excelOledbConnection.Close();
-
-                        // Read Data from First Sheet.
-                        excelOledbConnection.Open();
-                        excelDbCommand.CommandText = "SELECT * From [" + sheetName + "]";
-                        excelDataAdapter.SelectCommand = excelDbCommand;
-                        //Fill datatable from adapter
-                        excelDataAdapter.Fill(dt);
-                        excelOledbConnection.Close();
-                        foreach (DataRow r in dt.Rows)
-                        {
-                            string dtToParse = r[3].ToString().Split(' ')[0] + " " + r[4].ToString().Trim();
-                            if (dtToParse == " ") break;
-                            list.Add(
-                                new StockPrice()
-                                {
-                                    CompanyId = int.Parse(r[0].ToString().Trim()),
-                                    StockExchangeId = r[1].ToString().Trim(),   //some areas Id is string; some are int
-                                    Price = Convert.ToDouble(r[2].ToString().Trim()),
-                                    DateTime = DateTime.ParseExact(dtToParse, "dd-MM-yyyy HH:mm:ss", null)
-                                }
-                            );
-                            
-                        }
-
-                        context.StockPrice.AddRange(list);  //insert list of rows to table
-                        context.SaveChanges();
+                        string dtToParse = ew.Cells[row, 4].Value.ToString().Split(' ')[0] + " " + ew.Cells[row, 5].Value.ToString().Trim();
+                        if (dtToParse == " ") break;
+                        list.Add(
+                                  new StockPrice()
+                                  {
+                                      CompanyId = int.Parse(ew.Cells[row, 1].Value.ToString().Trim()),
+                                      StockExchangeId = ew.Cells[row, 2].Value.ToString().Trim(),
+                                      Price = Convert.ToDouble(ew.Cells[row, 3].Value.ToString().Trim()),
+                                      DateTime = DateTime.ParseExact(dtToParse, "dd-MM-yyyy HH:mm:ss", null)
+                                  }
+                              );
                     }
+                    context.StockPrice.AddRange(list);
+                    context.SaveChanges();
                 }
             }
         }
